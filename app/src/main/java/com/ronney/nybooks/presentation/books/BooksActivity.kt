4f4088f9b46d2e10bdb160ec682867aa728ea9ab
Ronney Nigro <ronneynigro@gmail.com>
2020-08-07 12:40:59 -0300

    Lista Mockada

--- app/src/main/java/com/ronney/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/ronney/nybooks/presentation/books/BooksActivity.kt
@@ -2,7 +2,10 @@ package com.ronney.nybooks.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ronney.nybooks.R
+import com.ronney.nybooks.data.model.Book
 import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
@@ -10,7 +13,21 @@ class BooksActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
 
-        toolbarMain.title = books_title
+        toolbarMain.title = getString(R.string.books_title)
         setSupportActionBar(toolbarMain)
+
+        with(recyclerBooks) {
+            layoutManager = LinearLayoutManager(this@BooksActivity, RecyclerView.VERTICAL, false)
+            setHasFixedSize(true)
+            adapter = BooksAdapter(getBooks())
+        }
+    }
+
+    fun getBooks(): List<Book> {
+        return listOf(
+                Book("Title 2", "Author 1"),
+                Book("Title 2", "Author 2"),
+                Book("Title 3", "Author 3")
+        )
     }
 }
---
+++ app/src/main/java/com/ronney/nybooks/presentation/books/BooksAdapter.kt
@@ -0,0 +1,36 @@
+package com.ronney.nybooks.presentation.books
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ronney.nybooks.R
+import com.ronney.nybooks.data.model.Book
+import kotlinx.android.synthetic.main.item_book.view.*
+
+class BooksAdapter(
+        private val books: List<Book>
+) : RecyclerView.Adapter<BooksAdapter.BookViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+        return BookViewHolder(view)
+    }
+
+    override fun getItemCount() = books.count()
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        holder.bindView(books[position])
+    }
+
+    class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val title = itemView.textTitle
+        private val author = itemView.textAuthor
+
+        fun bindView(book: Book) {
+            title.text = book.title
+            author.text = book.author
+        }
+    }
+}

d3f70f4f8a7c3cc2b77feb44b48b456fff754c99
Ronney Nigro <ronneynigro@gmail.com>
2020-11-13 08:17:57 -0300

    Pequenos ajustes... Falta ajustar navegabilidade

--- app/src/main/java/com/ronney/nybooks/presentation/movies/MoviesViewModel.kt
+++ app/src/main/java/com/ronney/nybooks/presentation/movies/MoviesViewModel.kt
@@ -10,7 +10,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MoviesViewModel : ViewModel() {
+class MoviesViewModel: ViewModel() {
 
     val moviesLiveData: MutableLiveData<List<Movie>> = MutableLiveData()
     val viewFlipperLiveData: MutableLiveData<Pair<Int, Int?>> = MutableLiveData()

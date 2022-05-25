20bbe4384cbe8faea45343b84f190540aa9eab66
Ronney Nigro <ronneynigro@gmail.com>
2022-05-25 13:01:58 -0300

    Atualizações de diversos plugins

--- app/src/main/java/com/ronney/nybooks/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/ronney/nybooks/presentation/base/BaseActivity.kt
@@ -3,12 +3,13 @@ package com.ronney.nybooks.presentation.base
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.include_toolbar.*
 
 open class BaseActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    protected fun setupToolBar(toolbar: androidx.appcompat.widget.Toolbar, titleIdRes: Int, showBackButton: Boolean = false) {
+    protected fun setupToolBar(toolbar: Toolbar, titleIdRes: Int, showBackButton: Boolean = false) {
         toolbar.title = getString(titleIdRes)
         setSupportActionBar(toolbarMain)
 
